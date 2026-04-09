264d37e1966795eb5db5add1611cd7ad85aa5564
您的姓名 <>
2026-04-09 15:12:06 +0800

    更新 vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,33 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://47.108.144.113:9111',
+      "/api": {
+        target: "http://47.108.144.113:9111",
         changeOrigin: true,
       },
-      '/oss': {
-        target: 'https://ruidao123.oss-cn-beijing.aliyuncs.com',
+      "/oss": {
+        target: "https://ruidao123.oss-cn-beijing.aliyuncs.com",
         changeOrigin: true,
         secure: true,
-        rewrite: (path) => path.replace(/^\/oss/, ''),
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/oss/, ""),
+      },
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
+});
